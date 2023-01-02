72233491e2d86b64683239567bc9c59def00d734
Aram Yesildeniz <aram.yesildeniz@outlook.com>
2023-01-02 21:43:03 +0100

    ok

--- metronome.js
+++ metronome.js
@@ -1,31 +1,34 @@
-// Get the UI elements
-const tempoInput = document.getElementById('tempo');
-const toggleButton = document.getElementById('toggle');
-
 // Flag for whether the metronome is currently running
 let isRunning = false;
 
 // Set up the audio context and oscillator
-const audioCtx = new AudioContext();
-const oscillator = audioCtx.createOscillator();
-oscillator.type = 'square';
-oscillator.frequency.value = 440;
-oscillator.start();
+let audioCtx;
+let oscillator;
+let gainNode;
 
-// Set up the gain node to control the volume
-const gainNode = audioCtx.createGain();
-gainNode.gain.value = 0;
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
+// Function for setting up the audio context and oscillator
+function setupAudio() {
+    audioCtx = new AudioContext();
+    oscillator = audioCtx.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.value = 440;
+    oscillator.start();
+
+    // Set up the gain node to control the volume
+    gainNode = audioCtx.createGain();
+    gainNode.gain.value = 0;
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+}
 
 // Function for starting the metronome
 function start() {
     // Calculate the interval between beats based on the tempo
-    const interval = (60 / tempoInput.value) * 1000;
+    const interval = (60 / document.getElementById('tempo').value) * 1000;
 
     // Start the metronome
     isRunning = true;
-    toggleButton.innerHTML = 'Stop';
+    document.getElementById('toggle').innerHTML = 'Stop';
     setInterval(() => {
         // Play a beat
         gainNode.gain.value = 1;
@@ -39,15 +42,18 @@ function start() {
 function stop() {
     // Stop the metronome
     isRunning = false;
-    toggleButton.innerHTML = 'Start';
+    document.getElementById('toggle').innerHTML = 'Start';
     clearInterval();
 }
 
 // Toggle the metronome when the button is clicked
-toggleButton.addEventListener('click', () => {
+document.getElementById('toggle').addEventListener('click', () => {
     if (isRunning) {
         stop();
     } else {
+        if (!audioCtx) {
+            setupAudio();
+        }
         start();
     }
 });
